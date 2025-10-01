325cc0b9307ef7031918eca58c33c352cb840200
TasiusKenways <88521859+tasiuskenways@users.noreply.github.com>
2025-10-01 16:49:55 +0700

    Configure compose app dependencies in build logic (#2)
    * Configure compose app dependencies in convention plugin
    
    * style: Make Android theme translucent
    
    This commit updates the Android application theme to be translucent.
    
    Key changes:
    - Created a new `themes.xml` file.
    - Defined `Theme.DailySleepTracker` with `android:windowIsTranslucent` set to `true`.
    - Updated `AndroidManifest.xml` to use the new `Theme.DailySleepTracker` theme.
    
    ---------
    
    Co-authored-by: Rezi Hardiansyah <rezi_hardiansyah@circles.asia>

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "my.id.tasius.dailysleeptracker.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradle)
+    compileOnly(libs.kotlin.gradle)
+    compileOnly(libs.compose.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("composeAppConvention") {
+            id = "dailysleeptracker.composeapp"
+            implementationClass = "my.id.tasius.dailysleeptracker.buildlogic.ComposeAppConventionPlugin"
+        }
+    }
+}
